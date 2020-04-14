fa097ac7bca21716908d357716a2de6da60949bb
Adam Swayne <MrSwayne@users.noreply.github.com>
2020-04-14 01:31:08 +0100

    Add files via upload

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,1 +1,14 @@
-from Core.Logger import LOGGER
+from Boards.BitBoard import *
+from Boards.ConnectBoard import *
+from Boards.TicBoard import *
+
+
+BOARDS = {"CONNECT4":ConnectBoard,
+          "C":ConnectBoard,
+          "C4":ConnectBoard,
+          "T":TicBoard,
+          "TIC":TicBoard,
+          "TICTACTOE":TicBoard}
+
+def get_board(txt):
+    return BOARDS[txt.upper()]
